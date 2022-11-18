00198225543694ef3b7cc55b0e28051005eeced3
Fadyorbae <fadilarahman111@gmail.com>
2022-11-18 11:06:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart' ;
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const MyApp()) ;
+
+class MyApp extends StatelessWidget {
+  const MyApp({key? key}) : super(key: key);
+
+  static const String_title = 'Flutter Tutorial' ;
+
+  @override 
+  Widget build (BuildContext context) {
+    return const MaterialApp (
+      title: _title, 
+      home: MyStatelelessWidget() , 
+    );
+  }
+}
+
+class MyStatelelessWidget extends StatelessWidget {
+  const MyStatelelessWidget ({Key? key}) : super(key: key);
+
+  @override 
+  widgetbuild(BuildContext context) {
+    return s
+  }
+}
